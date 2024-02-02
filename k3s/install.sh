a16be0375aa5358afbaf652003132337f605184c
André Penteado <andre.penteado@unesp.br>
2024-02-02 08:43:18 -0300

    Volumes criandos em single node em /var/local

--- k3s/install.sh
+++ k3s/install.sh
@@ -14,3 +14,24 @@ source <(kubectl completion bash)
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 source <(kubectl completion zsh)
 echo '[[ $commands[kubectl] ]] && source <(kubectl completion zsh)' >> ~/.zshrc
+
+# Rancher
+kubectl create namespace cattle-system
+helm install rancher rancher-latest/rancher  \
+     --namespace cattle-system               \
+     --set hostname=rancher.apcode.com.br    \
+     --set bootstrapPassword=passwebra       \
+     --set ingress.tls.source=secret
+
+# Caso de incopatibilidade de versão (substitua a versão para o correspondente downgrade)
+#wget https://github.com/k3s-io/k3s/releases/download/v1.27.10%2Bk3s1/k3s
+#cp k3s /usr/local/bin/
+#systemctl restart k3s
+#helm install rancher rancher-latest/rancher  \
+#     --namespace cattle-system               \
+#     --set hostname=rancher.apcode.com.br    \
+#     --set bootstrapPassword=passwebra       \
+#     --set ingress.tls.source=secret         \
+#     --set ingress.ingressClassName=traefik  \
+#     --set ingress.extraAnnotations.'traefik\.ingress\.kubernetes\.io/router.entrypoints'=websecure \
+#     --set ingress.extraAnnotations.'traefik\.ingress\.kubernetes\.io/router.tls'=true
